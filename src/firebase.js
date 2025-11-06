9172e390c6168385734146ba6ab98061345585ad
2concrete <danielpeace654@gmail.com>
2025-11-06 23:22:04 +0800

    implemented firebase

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD-bhm6o_65A771lHafebuQoQ_cMIdeelA",
+  authDomain: "minimalist-1.firebaseapp.com",
+  projectId: "minimalist-1",
+  storageBucket: "minimalist-1.firebasestorage.app",
+  messagingSenderId: "387448108801",
+  appId: "1:387448108801:web:411722f279224560b70d00",
+  measurementId: "G-8PXERBYNQD",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
